2e4bd52a079be4eb3ad42cfe6ce6ab2476d8d584
MoyuScript <i@moyu.moe>
2022-06-10 18:23:01 +0800

    fix: CSR 配置错误导致生产构建加载 CSS 失败

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,5 +1,8 @@
 const config = {
-  extends: ['@commitlint/config-conventional']
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'subject-case': [0],
+  },
 };
 
 module.exports = config;
